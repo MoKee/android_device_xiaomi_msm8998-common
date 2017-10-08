4432f78dc444adcb6a1be6ea2f752c946daf7d2d
xiaobai <fengz181x@126.com>
2017-10-08 19:02:48 +0800

    msm8998-common: remove sagit from build guard
    Change-Id: I3ead364a54678f38e395ad19024f585a4369f758

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter chiron sagit,$(TARGET_DEVICE)),)
+ifneq ($(filter chiron,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
