b38c078c3220f158cb815dd207877b26bfb9f0c6
Vladimir Oltean <olteanv@gmail.com>
2018-12-27 05:11:19 +0200

    msm8998-common: introduce fixup scripts and kang mode
    Also do the following:
    * Fix passing of arguments longer than one word, such as --section "Hello world"
    * Update the hashes and fixups for blobs kanged from bardock
    
    Wight554:
    * Small cosmetic changes
    * Fix some shellcheck warnings
    
    Change-Id: I917ed382a8c0e33ba7709151f2743a723dc50617
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,38 +22,38 @@ INITIAL_COPYRIGHT_YEAR=2017
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-MK_ROOT="$MY_DIR"/../../..
+MK_ROOT="${MY_DIR}/../../.."
 
-HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${MK_ROOT}/vendor/mk/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$MK_ROOT" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${MK_ROOT}" true
 
 # Copyright headers and guards
 write_headers "chiron sagit"
 
 # The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
 
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${MK_ROOT}" false
 
     # Copyright headers and guards
     write_headers
 
     # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
+    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
     # Finish
     write_footers
