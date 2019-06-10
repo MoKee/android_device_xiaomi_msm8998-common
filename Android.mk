37585a0f5e30ea222ff9cf29009b10ba72f5c075
Martincz Gao <martincz@mokeedev.com>
2019-06-10 18:08:47 +0800

    msm8998-common: Don't include sagit
    Change-Id: Idccfdd18f41ad2a6f39853395a966aff02d05f0b

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter sagit chiron,$(TARGET_DEVICE)),)
+ifneq ($(filter chiron,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
